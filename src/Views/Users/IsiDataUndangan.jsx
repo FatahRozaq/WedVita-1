ba192e3ce82e1891a470018f61a40ea5a916eb59
FatahRozaq <fatah.rozaq20@gmail.com>
2023-10-18 06:07:39 +0700

    Update Pesanan

--- src/Views/Users/IsiDataUndangan.jsx
+++ src/Views/Users/IsiDataUndangan.jsx
@@ -24,7 +24,8 @@ function IsiDataUndangan() {
     const motherOfBrideRef = createRef();
     const accountNumberRef = createRef();
 
-    const {user, token, setUser, setToken, setWeddingData} = useStateContext();
+    const { id } = useParams();
+    const { user, token, setUser, setToken, setWeddingData } = useStateContext();
 
     const [errors, setErrors] = useState(null);
     const [selectedImageGroom, setSelectedImageGroom] = useState(null);
@@ -37,12 +38,12 @@ function IsiDataUndangan() {
 
     const onLogout = ev => {
         ev.preventDefault()
-    
+
         axiosClient.post('/logout')
-          .then(() => {
-            setUser({})
-            setToken(null)
-          })
+            .then(() => {
+                setUser({})
+                setToken(null)
+            })
     }
 
     if (!token) {
@@ -50,12 +51,14 @@ function IsiDataUndangan() {
     }
 
     useEffect(() => {
-        
+
         axiosClient.get('/user').then(({ data }) => {
-          setUser(data);
+            setUser(data);
         });
-    
-      }, [token, setUser, setToken]);
+
+        // console.log(id)
+
+    }, [token, setUser, setToken]);
 
     const handleImageChangeGroom = (e) => {
         if (e.target.files.length > 0) {
@@ -65,13 +68,13 @@ function IsiDataUndangan() {
 
     const handleImageChangeBride = (e) => {
         if (e.target.files.length > 0) {
-          setSelectedImageBride(e.target.files[0]);
+            setSelectedImageBride(e.target.files[0]);
         }
     };
 
     const handleImageChangeCover = (e) => {
         if (e.target.files.length > 0) {
-          setSelectedImageCover(e.target.files[0]);
+            setSelectedImageCover(e.target.files[0]);
         }
     };
 
@@ -85,10 +88,11 @@ function IsiDataUndangan() {
 
     const onSubmit = (ev) => {
         ev.preventDefault()
-      
+
+        // navigate(`/pesanan/${user.id}`);
         const formData = new FormData();
         formData.append('userId', user.id);
-        formData.append('designId', 14);
+        formData.append('designId', id);
         formData.append('groomName', groomNameRef.current.value);
         formData.append('brideName', brideNameRef.current.value);
         formData.append('groomPhoto', selectedImageGroom);
@@ -105,22 +109,23 @@ function IsiDataUndangan() {
         formData.append('accountNumber', accountNumberRef.current.value);
 
         axiosClient.post('/invitations', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        })
-        .then(({data}) => {
-          setWeddingData(data.weddingInvitations)
-          navigate('/cms-desain-ku');
-        })
-        .catch(err => {
-          const response = err.response;
-          if( response && response.status === 422){
-            // response.data.errors
-            // console.log(response.data.errors);
-            setErrors(response.data.errors)
-          }
-        })
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            })
+            .then(({ data }) => {
+                setWeddingData(data.weddingInvitations)
+                //   return <Navigate to={"/cms-desain-ku"} />
+                //   navigate('/cms-desain-ku');
+            })
+            .catch(err => {
+                const response = err.response;
+                if (response && response.status === 422) {
+                    // response.data.errors
+                    // console.log(response.data.errors);
+                    setErrors(response.data.errors)
+                }
+            })
 
 
     }
@@ -130,47 +135,45 @@ function IsiDataUndangan() {
             <div class="text-xl font-extrabold mb-4">Form Data Kartu Undangan</div>
             <div className='DataKartuUndangan'>
 
-                {errors && 
-                  <div>
-                    {Object.keys(errors).map(key => (
-                      <p key={key}>{errors[key][0]}</p>
-                    ))}
-                  </div>
+                {errors &&
+                    <div>
+                        {Object.keys(errors).map(key => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
                 }
 
 
                 <form onSubmit={onSubmit}>
-                <div class="bg-white p-4 mb-4 rounded shadow">
-                    <div class="mb-4">
-
-                            <div className="mb-6 flex justify-between">
-                                <div className="w-1/3">
+                    <div class="bg-white p-4 mb-4 rounded shadow">
+                        <div class="mb-4">
+                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 mb-6">
+                                <div>
                                     <label for="groomName" class="block mb-2 text-sm font-semibold text-gray-900">Nama Pengantin Pria</label>
                                     <input ref={groomNameRef} name="groomName" type="name" id="groomName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Pengantin Pria" required></input>
                                 </div>
-                                
-                                <div className="w-1/3">
+                                <div>
                                     <label for="brideName" class="block mb-2 text-sm font-semibold text-gray-900">Nama Pengantin Wanita</label>
                                     <input ref={brideNameRef} name="brideName" type="name" id="brideName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Pengantin Wanita" required></input>
                                 </div>
                                 <div>
-                                    <label for="fatherOfGroom" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ayah</label>
+                                    <label for="fatherOfGroom" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ayah Mempelai Pria</label>
                                     <input ref={fatherOfGroomRef} name="fatherOfGroom" type="name" id="fatherOfGroom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ayah Pengantin Pria" required></input>
                                 </div>
 
                                 <div>
-                                    <label for="motherOfGroom" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ibu</label>
-                                    <input ref={motherOfGroomRef} name="fatherOfGroom" type="name" id="fatherOfGroom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ayah Pengantin Pria" required></input>
+                                    <label for="fatherOfBride" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ayah Mempelai Wanita</label>
+                                    <input ref={fatherOfBrideRef} name="fatherOfBride" type="name" id="fatherOfBride" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ayah Pengantin Wanita" required></input>
                                 </div>
 
                                 <div>
-                                    <label for="fatherOfBride" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ayah</label>
-                                    <input ref={fatherOfBrideRef} name="fatherOfGroom" type="name" id="fatherOfGroom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ayah Pengantin Pria" required></input>
+                                    <label for="motherOfGroom" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ibu Mempelai Pria</label>
+                                    <input ref={motherOfGroomRef} name="motherOfGroom" type="name" id="motherOfGroom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ibu Pengantin Pria" required></input>
                                 </div>
 
                                 <div>
-                                    <label for="motherOfBride" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ibu</label>
-                                    <input ref={motherOfBrideRef} name="motherOfGroom" type="name" id="motherOfGroom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ibu Pengantin Pria" required></input>
+                                    <label for="motherOfBride" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ibu Mempelai Wanita</label>
+                                    <input ref={motherOfBrideRef} name="motherOfBride" type="name" id="motherOfBride" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ibu Pengantin Wanita" required></input>
                                 </div>
                                 <div>
                                     <label for="groomPhoto" class="block mb-2 text-sm font-semibold text-gray-900">Foto Pengantin Pria</label>
@@ -192,41 +195,41 @@ function IsiDataUndangan() {
 
                             </div>
 
+                            {/* 
                             <div className="mb-6 flex justify-between">
                                 <div className="w-1/3">
-                                    <label for="fatherOfGroom" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ayah</label>
-                                    <input ref={fatherOfGroomRef} name="fatherOfGroom" type="name" id="fatherOfGroom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ayah Pengantin Pria" required></input>
+
                                 </div>
-                                
+
                                 <div className="w-1/3">
-                                    <label for="fatherOfBride" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ayah</label>
-                                    <input ref={fatherOfBrideRef} name="fatherOfBride" type="name" id="fatherOfBride" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ayah Pengantin Wanita" required></input>
+
+                                </div>
+
+                            </div>
+
+                            <div className="mb-6 flex justify-between">
+                                <div className="w-1/3">
+                                </div>
+
+                                <div className="w-1/3">
                                 </div>
                             </div>
 
                             <div className="mb-6 flex justify-between">
                                 <div className="w-1/3">
-                                    <label for="motherOfGroom" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ibu</label>
-                                    <input ref={motherOfGroomRef} name="motherOfGroom" type="name" id="motherOfGroom" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ibu Pengantin Pria" required></input>
                                 </div>
-                                
+
                                 <div className="w-1/3">
-                                    <label for="motherOfBride" class="block mb-2 text-sm font-semibold text-gray-900">Nama Ibu</label>
-                                    <input ref={motherOfBrideRef} name="motherOfBride" type="name" id="motherOfBride" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Nama Ibu Pengantin Wanita" required></input>
                                 </div>
                             </div>
 
                             <div className="mb-6 flex justify-between">
                                 <div className="w-1/3">
-                                    <label for="groomPhoto" class="block mb-2 text-sm font-semibold text-gray-900">Foto Pengantin Pria</label>
-                                    <input ref={groomPhotoRef} name="groomPhoto" type="file" id="groomPhoto" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" accept="image/*" onChange={handleImageChangeGroom} required></input>
                                 </div>
-                                
+
                                 <div className="w-1/3">
-                                    <label for="bridePhoto" class="block mb-2 text-sm font-semibold text-gray-900">Foto Pengantin Wanita</label>
-                                    <input ref={bridePhotoRef} name="bridePhoto" type="file" id="bridePhoto" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" accept="image/*" onChange={handleImageChangeBride} required></input>
                                 </div>
-                            </div>
+                            </div> */}
 
                             <div class="mb-6">
                                 <label for="weddingDate" class="block mb-2 text-sm font-semibold text-gray-900">Tanggal Pernikahan</label>
@@ -258,11 +261,11 @@ function IsiDataUndangan() {
                                 <label for="weddingMap" class="block mb-2 text-sm font-semibold text-gray-900">Link Google Maps Pernikahan</label>
                                 <input ref={weddingMapRef} name="weddingMap" type="name" id="weddingMap" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Masukan Link Google Maps Pernikahan" required></input>
                             </div>
-                 
+
                             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Upload</button>
-                        
+
+                        </div>
                     </div>
-                </div>
                 </form>
             </div>
         </Layout>
