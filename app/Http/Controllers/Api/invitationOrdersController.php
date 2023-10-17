064538d3dbeaf3607951e7342e82e5133b31203f
Brahmantya Ndaru Taja Bagus Santoso <brahmantyasantoso5@gmail.com>
2023-10-17 16:15:07 +0700

    whitespace

--- app/Http/Controllers/Api/invitationOrdersController.php
+++ app/Http/Controllers/Api/invitationOrdersController.php
@@ -30,4 +30,35 @@ class invitationOrdersController extends Controller
             'invitationOrders' => $invitationOrders,
         ], 201);
     }
+
+    public function update(invitationOrdersRequest $request, invitationOrders $invitationOrders)
+    {
+        $validatedData = $request->validated();
+
+        $invitationOrders->update([
+            'designId' => $validatedData['designId'],
+            'userId' => $validatedData['userId'],
+            'invitationId' => $validatedData['invitationId'],
+            'orderDate' => $validatedData['orderDate'],
+            'orderExpired' => $validatedData['orderExpired'],
+            'totalPrice' => $validatedData['totalPrice'],
+            'orderStatus' => $validatedData['orderStatus'],
+        ]);
+
+        return response()->json([
+            'message' => 'Data wedding invitation berhasil diperbarui',
+            'invitationOrders' => $invitationOrders,
+        ], 200);
+    }
+
+
+    public function destroy(invitationOrders $invitationOrders)
+    {
+        $invitationOrders->delete();
+
+        return response()->json([
+            'message' => 'Data wedding invitation berhasil dihapus',
+        ], 200);
+    }
+
 }
--- app/Models/weddingPhotos.php
+++ app/Models/weddingPhotos.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class weddingPhotos extends Model
-{   
+{
 
     protected $table = 'weddingPhotos';
     use HasFactory;
